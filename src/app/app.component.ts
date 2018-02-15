e8d51ddf1f9d1d0752f3b4f3eca015a81ddb9262
Bruno Pimentel <brunoapimentel@gmail.com>
2018-02-15 20:55:56 -0200

    Teste de API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  personagem = {};
+
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit(){
+    this.http.get('https://swapi.co/api/people/1/').subscribe((dados) => {
+      this.personagem = dados;
+    });
+  }
 }
