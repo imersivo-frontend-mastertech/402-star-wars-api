4c0786245acd9d5c2d2c34957fffb65efe6f0bc6
Bruno Pimentel <brunoapimentel@gmail.com>
2018-02-15 21:54:34 -0200

    Adiciona paginação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,32 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'app';
   personagens;
+  proxima;
+  anterior;
 
   constructor(private http: HttpClient){
   }
 
   ngOnInit(){
     this.http.get<any>('https://swapi.co/api/people/').subscribe((dados) => {
+      this.proxima = dados.next;
+      this.anterior = dados.previous;
+      this.personagens = dados.results;
+    });
+  }
+
+  avancar(){
+    this.http.get<any>(this.proxima).subscribe((dados) => {
+      this.proxima = dados.next;
+      this.anterior = dados.previous;
+      this.personagens = dados.results;
+    });
+  }
+
+  retroceder(){
+    this.http.get<any>(this.anterior).subscribe((dados) => {
+      this.proxima = dados.next;
+      this.anterior = dados.previous;
       this.personagens = dados.results;
     });
   }
